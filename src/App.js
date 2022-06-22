67b9394b1c3b004c0b9fd723b33b35ebd0b45cf2
Pawel <pawel.aleksander@gmail.com>
2022-06-22 13:21:27 +0200

    featch json data from reddit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import { useState, useEffect } from 'react';
-import Article from './component/Article';
+import { useState, useEffect } from "react";
+
+import Article from "./component/Article";
 
 function App() {
-const [articles, setArticles] = useState([]);
-const [subReddit, setSubReddit] = useState('webdev');
+    const [articles, setArticles] = useState([]);
+    const [subReddit, setSubReddit] = useState("webdev");
 
-useEffect(() => {
-fetch(`https://www.reddit.com/r/webdev.json`).then(res => {
-  if (res.status != 200) {
-    console.log(`erooror`);
-    return;
-  }
-  res.json().then(data => {
-    if (data != null){
-      setArticles(data.data.children);
-    }
-  }
-  )
-
-})}, [subReddit]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <input type="text" className="input" value='webdev'/>
-      </header>
-      <div className="articles">
-
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        fetch(`https://www.reddit.com/r/webdev.json`).then((res) => {
+            if (res.status !== 200) {
+                console.log(`erooror`);
+                return;
+            }
+            res.json().then((data) => {
+                if (data != null) {
+                    setArticles(data.data.children);
+                }
+            });
+        });
+    }, [subReddit]);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input type="text" className="input" value="webdev" />
+            </header>
+            <div className="articles">
+                    
+                    {
+                        (articles !== null) ? articles.map((article, index) => <Article key={index} article={article.data} /> ) : ''
+                    }
+                    
+            </div>
+        </div>
+    );
 }
 
 export default App;
