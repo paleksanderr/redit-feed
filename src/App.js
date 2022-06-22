0a4ef574209a131d7366bc2c8409f6f2c41f2418
Pawel <pawel.aleksander@gmail.com>
2022-06-22 12:18:48 +0200

    adding progres

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
+import { useState, useEffect } from 'react';
 
 function App() {
+const [articles, setArticles] = useState([]);
+const [subReddit, setSubReddit] = useState('webdev');
+
+useEffect(() => {
+fetch(`https://www.reddit.com/r/webdev.json`).then(res => {
+  if (res.status !== 200) {
+    console.log(`erooror`);
+    return;
+  }
+  res.json().then(data => {
+    if (data != null){
+      console.log(data)
+    }
+  }
+  );
+
+});}, [subReddit]);
+
   return (
     <div className="App">
       <header className="App-header">
         <input type="text" className="input" value='webdev'/>
       </header>
       <div className="articles">
-        
+
       </div>
     </div>
   );
